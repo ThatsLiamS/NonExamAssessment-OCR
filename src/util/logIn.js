7dda227cf9846c18b181033deff2e5ffddd1530c
Liam Skinner <liamdavidskinner@gmail.com>
2022-05-03 17:34:27 +0100

    feat(logIn): created login and sign up system
    - Updated validate.js values
    - Installed inquirer
    - Added Firestore login and sign up system

---
+++ src/util/logIn.js
@@ -0,0 +1,101 @@
+/* Import packages and files */
+const inquirer = require('inquirer');
+const validate = require('./validate.js');
+const defaultData = require('./../database.json');
+
+
+/* Allows the user to log in and return user object */
+const logIn = async (firestore) => {
+
+	return await inquirer
+		.prompt([{ name: 'email', type: 'input' }, { name: 'password', type: 'password' }])
+		.then(async (answers) => {
+
+			/* validate user input, returns boolean */
+			const email = validate({ value: answers['email'], type: { name: 'email', minLength: 5, maxLength: 50 } });
+			const password = validate({ value: answers['password'], type: { name: 'password', maxLength: 25 } });
+
+			if (email == false || password == false) {
+				console.log('Invalid email or password. Try again!\n');
+				return await logIn(firestore);
+			}
+
+			/* Searches the database for the user's account */
+			let found = false;
+			const players = await firestore.collection('players').get();
+			players.forEach(async doc => {
+
+				/* If the login data matches, return user object */
+				if (doc.data().email == answers['email'] && doc.data().password == answers['password']) found = doc.data();
+
+			});
+
+			if (!found) {
+				console.log('Invalid email or password. Try again!\n');
+				return await logIn(firestore);
+			}
+
+			/* returns userData */
+			return found;
+
+		});
+
+};
+
+
+/* Custom ID generator */
+const generator = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+
+/* Signs a user up to the program */
+const signUp = async (firestore) => {
+
+	return await inquirer
+		.prompt([
+			{ name: 'name', type: 'input' },
+			{ name: 'date of birth', type: 'input' },
+
+			{ name: 'email', type: 'input' },
+
+			{ name: 'password', type: 'password' },
+			{ name: 'confirm password', type: 'password' }
+		])
+		.then(async (answers) => {
+
+			/* validate user input, returns boolean */
+			const name = validate({ value: answers['name'], type: { name: 'string', minLength: 5, maxLength: 25 } });
+			const DoB = true;
+			// validate({ value: answers['date of borth'], type: { name: 'date' } });
+
+			const email = validate({ value: answers['email'], type: { name: 'email', minLength: 5, maxLength: 50 } });
+			if (!email) {
+				console.log('Invalid email provided. Try again!');
+				return await signUp(firestore);
+			}
+
+			const password1 = validate({ value: answers['password'], type: { name: 'password', maxLength: 25 } });
+			const password2 = validate({ value: answers['confirm password'], type: { name: 'password', maxLength: 25 } });
+			if (!password1 || !password2 || answers['password'] != answers['confirm password']) {
+				console.log('Invalid password provided. Try again!');
+				return await signUp(firestore);
+			}
+
+			/* Set the data */
+			const userData = defaultData.player;
+			userData.id = `${generator()}-${generator()}-${generator()}`;
+			userData.name = answers['name'];
+			userData.dob = answers['date of birth'];
+
+			userData.email = answers['email'];
+			userData.password = answers['password'];
+
+			await firestore.doc(`/guilds/${userData.id}/`).set(userData);
+
+			/* Return userData */
+			return userData;
+		});
+};
+
+module.exports = {
+	logIn,
+	signUp,
+};
--- src/util/validate.js
+++ src/util/validate.js
@@ -3,32 +3,32 @@ const regex = {
 	email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-._]+)$/,
 	string: /^[a-zA-Z-_]$/,
 	number: /^[0-9]$/,
-	alphanumeric: /^[a-zA-Z0-9]$/
+	password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/
 };
 
-module.exports = ({ type, text: value }) => {
+module.exports = ({ type, value }) => {
 
 	/* Does text meet the expression */
 	const expression = regex[type.name];
-	if (!value.test(expression)) return false;
+	if (!expression.test(value)) return false;
 
 	/* Extra validation conditions */
 	switch (type.name) {
 
 	case "email":
-		if (typeof value !== String || value.length < type.minLength || value.length > type.maxLength) return false;
+		if (typeof value !== 'string' || value.length < type.minLength || value.length > type.maxLength) return false;
 		break;
 
 	case "string":
-		if (typeof value !== String || value.length < type.minLength || value.length > type.maxLength) return false;
+		if (typeof value !== 'string' || value.length < type.minLength || value.length > type.maxLength) return false;
 		break;
 
 	case "number":
 		if (Number(value) == 'NaN') return false;
 		break;
 
-	case "alphanumeric":
-		if (typeof value !== String || value.length < type.minLength || value.length > type.maxLength) return false;
+	case "password":
+		if (typeof value !== 'string' || value.length < 8 || value.length > type.maxLength) return false;
 		break;
 
 	}
